9ef0ac472ca50c182e8675ddd59317c751ebf89b
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-02-20 15:32:40 -0800

    Submission for tested PP2

--- PP2/shift.c
+++ PP2/shift.c
@@ -2,9 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
+int shiftstring(char* s, int numshift){
+    int length = strlen(s);
+    if (length == 0 || numshift == 0) {
+        fprintf(stdout,"%s\n", s);
+        return 0;
+    }
+    numshift = numshift % length;
+    char temp1;
+    for (int y = 0; y < numshift; y++) {
+        temp1 = s[length-1];
+        for (int x = (length-1); x > 0; x--) {
+            s[x] = s[x-1];
+        }
+        s[0] = temp1;
+        }
+
+    // for (int t = 0; t < length; t++){
+    //     fprintf(stdout, "Character at index %d: %c\n", t, s[t]);
+    //     fprintf(stdout, "%c",s[t]);
+    // }
+
+    fprintf(stdout,"%s\n", s);
+    return 0;
+}
+
 int main(int argc, char *argv[]){
-    printf("argc = %d\n", argc);
-    printf("argv[0] - %s\n", argv[0]);
+    // printf("argc = %d\n", argc);
+    // printf("argv[0] - %s\n", argv[0]);
 
     if (argc != 3) {
         fprintf(stderr, "Argument count must be 3\n");
@@ -19,25 +44,8 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
-    int length = strlen(s);
 
- 
-    char temp1;
-
-
-    for (int y = 0; y < shifts; y++) {
-        temp1 = s[length-1];
-        for (int x = (length-1); x > 0; x--) {
-            s[x] = s[x-1];
-        }
-        s[0] = temp1;
-        }
-
-    for (int t = 0; t < length; t++){
-        fprintf(stdout, "Character at index %d: %c\n", t, s[t]);
-    }
-
-
+    shiftstring(s, shifts);
     free(s);
     return 0;
 }
