66e93430cd4443a939424cbce0ff643960bb4e59
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-02-26 22:24:39 -0800

    PP3 Init

---
+++ PP3/matrixproduct.cpp
@@ -0,0 +1,33 @@
+// Write a C++ program that multiplies INTEGER matrix by a vector given to you in two files whose names will be passed via command line arguments, your program must print out the output vector to STDOUT in the format displayed below 
+
+// IMPORTANT NOTE: we are defining the format of input matrix to have a first line that includes the number of rows and columns in the first line. The output format is not changed, just the resulting matrix should be output. for the vector file the first line has the number of rows in the vector. This should make your job easier.
+
+// matrix.txt
+
+// 2 3
+
+// 1 2 1
+
+// 2 1 2
+
+// vector.txt
+
+// 3 
+
+// 1 
+
+// 1
+
+// 1 
+
+// matrixproduct -m matrix.txt -v vector.txt
+
+// 4
+
+// 5
+
+// Note any type of space can be used as separator. Must use streams for input output and as much use of C++ as possible, i.e. use of printf instead of cout will result in deductions from grade. 
+
+int main(int argc, char* argv[]){
+
+}
