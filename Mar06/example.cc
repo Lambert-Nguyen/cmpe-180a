4e9ff132874fa8046370d880ae94e674f53a2bb1
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-03-06 19:06:06 -0800

    operator overloading examples

--- Mar06/example.cc
+++ Mar06/example.cc
@@ -1,3 +1,13 @@
-    double XYPoint::operator*(const XYPoint &o){
+#include "example.h"
+
+    double XYPoint::operator*(const XYPoint &o) const{
         return (o.x * x + o.y * y);
+    }
+    bool XYPoint::operator!()const{
+        return(!x&&!y);
+    }
+    iostream& operator<<(iostream &o, const XYPoint &p){
+        o<<"x="<<p.x<<endl;
+        o<<"y="<<p.y<<endl;
+        return o;
     }
--- Mar06/example.h
+++ Mar06/example.h
@@ -1,4 +1,8 @@
+#include <iostream>
+using std::iostream;
 class XYPoint{
     public:
     double operator*(const XYPoint &o);
+    bool operator!();
+    friend iostream& operator<<(iostream &o, const XYPoint &p);
 }
