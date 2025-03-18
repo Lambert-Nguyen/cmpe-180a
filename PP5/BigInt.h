b745a9e51fd2fd2038cb011e76b7bf424f213c5b
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-03-18 01:19:54 -0700

    add header definition for BigInt class and operator overloading declarations

--- PP5/BigInt.h
+++ PP5/BigInt.h
@@ -1,8 +1,38 @@
 #ifndef BIGINT_H
 #define BIGINT_H
 
+#include <iostream>
+#include <vector>
+#include <string>
+
 class BigInt{
-    
+    private:
+    std::vector<int> digit;
+    bool isNegative;
+
+    public: 
+    BigInt();
+    BigInt(const std::vector<int> &digit);
+    BigInt(const char num[], size_t size);
+
+    BigInt operator+(const BigInt& other) const;
+    BigInt operator-(const BigInt& other) const;
+    BigInt operator*(const BigInt& other) const;
+    BigInt operator!() const;
+
+    bool operator==(const BigInt& other) const;
+    bool operator<(const BigInt& other) const;
+    bool operator>(const BigInt& other) const;
+    bool operator<=(const BigInt& other) const;
+    bool operator>=(const BigInt& other) const;
+
+    BigInt& operator++();  // Pre-increment
+    BigInt operator++(int); // Post-increment
+    BigInt& operator--();  // Pre-decrement
+    BigInt operator--(int); // Post-decrement
+
+    friend std::ostream& operator<<(std::ostream& out, const BigInt& num);
+    friend std::istream& operator>>(std::istream& in, BigInt& num);
 };
 
 #endif
