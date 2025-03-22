361f98d700eb2806e0b9e57ff83a7d6bbb5861cb
Nguyen Phuong Duy Lam <nguyenphuongduylam@gmail.com>
2025-03-21 22:34:05 -0700

    Singly Linked List implementation

---
+++ misc/quiz6/SinglyLinkedList.h
@@ -0,0 +1,34 @@
+// Create (DEFINE AND IMPLEMENT--MUST BE SEPARATED) a single Linked list class named SinglyLinkedList: 
+// support the following methods:
+// insertAtHead(), insertAtTail(), size(), empty(), insertAtPosition(n), getPosition(n). 
+// You must create the proper prototypes and use the right arguments with correct const qualifiers.
+// Make good decisions on what to do on error conditions, use exceptions instead of return codes
+
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+
+#include <cstddef>
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(){
+        data = 0;
+        next = NULL;
+    }
+    Node(int data_i){
+        this->data=data_i;
+        this->next=NULL;
+    }
+};
+
+class SinglyLinkedList {
+    private:
+    Node* head;
+    int listSize;
+    public:
+};
+
+#endif
